db59db2bdaf3c90b1771eb2a110bc693482b8715
A8223079 <101238663+A8223079@users.noreply.github.com>
2022-06-05 18:46:19 +0800

    Update OOXX.h

--- OOXX.h
+++ OOXX.h
@@ -1,1 +1,19 @@
+#ifndef OOXX_H
+#define OOXX_H
+#include<string>
+using namespace std;
+class OOXX
+{
+private:
+	int PlayerInput;
+	int round = 1;
+	string ChartNumber[9] = { "1","2","3","4","5","6","7","8","9" };
+public:
+	OOXX();
+	OOXX(int);
+	void GameChart();
+	void ChooseCoordinateO();
+	void Victory();
 
+};
+#endif
